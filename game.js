6559f2bf7a9dcb49e89a21413a4e20b206dce8f6
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-07-27 20:57:19 +0530

    Added enemy and radom respwan

--- game.js
+++ game.js
@@ -5,12 +5,14 @@ var ship_js = document.getElementById("Ship");
 ship_js.style.top = "75%";
 ship_js.style.left = "50%";
 
+var enemy_js = document.getElementById("Enemy");
+enemy_js.style.top = getRandomInt(10,50)+"%";
+enemy_js.style.left = getRandomInt(5,90)+"%";
+
 document.onkeydown = arrowKey;
 
 function arrowKey(e) {
-
     e = e || window.event;
-
     if (e.keyCode == '38') {    // up arrow
         ship_js.style.top = parseInt(ship_js.style.top)-1+"%";
     }
@@ -24,4 +26,13 @@ function arrowKey(e) {
         ship_js.style.left = parseInt(ship_js.style.left)+1+"%";
     }
 
+}
+
+function getRandomInt(min,max) {
+    var temp = Math.floor(Math.random() * max);
+
+    if(temp>=min)
+        return temp;
+    else 
+        return getRandomInt(min,max);
 }
