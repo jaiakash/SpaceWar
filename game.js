0fac5592f37a2de79daa8bba4f4d3c75c8fa06a5
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-07-27 22:49:16 +0530

    Added time counter

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-document.getElementById("time").innerHTML = "Time : 0";
-document.getElementById("score").innerHTML = "Score : 0";
+var time = 0;
+var score = 0;
 
 var ship_js = document.getElementById("Ship");
 ship_js.style.top = "75%";
@@ -10,6 +10,29 @@ enemy_js.style.top = getRandomInt(10,50)+"%";
 enemy_js.style.left = getRandomInt(5,90)+"%";
 
 document.onkeydown = arrowKey;
+
+gameLoop();
+
+function gameLoop() {
+    document.getElementById("time").innerHTML = "Time : "+time.toFixed(2);
+    document.getElementById("score").innerHTML = "Score : "+score;
+    time+=0.01;
+
+    // highScore();
+    // document.getElementById("board").innerHTML = "Score : "+score.toFixed(1);
+    // //create a new block, if old one is lost
+    // if(rectX<0){
+    //     speed+=0.5;
+    //     rectX = 550;
+    //     randomY = (Math.random()<0.5) ? 5:65;
+    // }
+
+    // collision_detector();
+    // draw();
+
+    window.requestAnimationFrame(gameLoop);
+}
+
 
 function arrowKey(e) {
     e = e || window.event;
